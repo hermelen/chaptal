5dc8709f0bd75ab419855100d403d0690e627ef7
hermelen <hermelen.peris@gmail.com>
2020-03-05 00:23:29 +0100

    first commit

---
+++ js/data.js
@@ -0,0 +1,83 @@
+var max = 890;
+
+var pupils = [
+   {
+      "name":"Jean-Mich",
+      "age":18,
+      "rates":[
+         15,
+         12,
+         5,
+         15,
+         11
+      ],
+      "picture":Math.floor(Math.random() * max) + 1
+   },
+   {
+      "name":"Jean-Claude",
+      "age":17,
+      "rates":[
+         18,
+         12,
+         8,
+         12,
+         11
+      ],
+      "picture":Math.floor(Math.random() * max) + 1
+   },
+   {
+      "name":"Jean-Pierre",
+      "age":15,
+      "rates":[
+         10,
+         10,
+         11,
+         9,
+         11
+      ],
+      "picture":Math.floor(Math.random() * max) + 1
+   },
+   {
+      "name":"Claudine",
+      "age":22,
+      "rates":[
+         19,
+         12,
+         5,
+         17,
+         12
+      ],
+      "picture":Math.floor(Math.random() * max) + 1
+   },
+   {
+      "name":"Nadine",
+      "age":17,
+      "rates":[
+         19,
+         5,
+         3,
+         4,
+         14
+      ],
+      "picture":Math.floor(Math.random() * max) + 1
+   },
+   {
+      "name":"Micheline",
+      "age":18,
+      "rates":[
+         2,
+         18,
+         14,
+         12,
+         13
+      ],
+      "picture":Math.floor(Math.random() * max) + 1
+   },
+]
+
+var school = {
+   "name":"Lycée Chaptal",
+   "city":"Mende",
+   "effectif": 1000,
+   "date": "05 Septembre 1975"
+}
---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    console.log( "ready!" );
+    for (let index = 0; index < pupils.length; index++) {
+        var totalNotes = 0
+        for (let indexBis = 0; indexBis < pupils[index].rates.length; indexBis++) {
+            totalNotes += pupils[index].rates[indexBis];
+        }
+        var average = totalNotes / pupils[index].rates.length;
+        var random = pupils[index].picture.toString();
+        if (random.length !== 3) {
+            console.log('too short');
+            for (let indexTer = 0; indexTer < (3 - random.length); indexTer++) {
+                random += "0"
+            }
+        }
+        $(".pupils").append(
+        `
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${random}.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">${pupils[index].name}</h5>
+                    <p class="card-text">Age : ${pupils[index].age}</p>
+                    <p class="card-text">Moyenne : ${average}</p>
+                    <a target="_blank" href="https://fr-fr.facebook.com/pages/category/High-School/Lyc%C3%A9e-Chaptal-Mende-1957123361195357/" class="btn btn-primary">${pupils[index].name} Profile</a>
+                </div>
+            </div>
+            `
+        );
+        $(".nav").append(
+            `<li>${pupils[index].name}</li>`
+        )     
+    }
+    // console.log(Math.floor(Math.random() * 890) + 1)
+});
